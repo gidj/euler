f239f28e8ca10d3e22ea43526dae7c4bc961f544
Gideon VanRiette <gvanriette@gmail.com>
2016-03-06 15:40:07 -0600

    Start implementing problem 1 in Ruby

---
+++ ruby/001.rb
@@ -0,0 +1,61 @@
+#!/usr/local/bin/ruby
+
+
+class FactorSum
+  attr_accessor :factors
+
+  @factors = []
+  @highest = 0
+  @sum = nil
+
+  class << self
+    def imperative
+      new(factors, highest, 'imperative')
+    end
+    def functional
+      new(factors, highest, 'functional')
+    end
+    def algebraic
+      new(factors, highest, 'algebraic')
+    end
+  end
+
+  def initizialize (factors = 3, highest = 1000, algorithm = 'imperative')
+    if factors.nil?
+      raise Error
+    elsif factors.respond_to?(:each)
+      factors.each do |factor|
+        @factors << factor
+      end
+    else
+      @factors << factors
+    end
+
+    if highest.nil?
+      raise Error
+    elsif higest.respond_to?(:each)
+      raise Error
+    else
+      @highest = highest
+    end
+
+    case algorithm
+    when 'imperative' then @sum = imperative_sum
+    when 'functional' then @sum = functional_sum
+    when 'algebraic' then @sum = algebraic_sum
+    else @sum = algebraic_sum
+    end
+  end
+
+  def imperative_sum
+    total = 0
+  end
+
+  def functional_sum
+  end
+
+  def algebraic_sum
+  end
+
+  private :new, :imperative_sum, :functional_sum, :algebraic_sum
+end
