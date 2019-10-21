eb06cb2ce2714fa2d54503dbb47581f0373eee7b
Gideon VanRiette <gvanriette@gmail.com>
2019-10-21 15:17:12 -0500

    Fix for python 3

--- python/001.py
+++ python/001.py
@@ -9,11 +9,13 @@ get 3, 5, 6 and 9. The sum of these multiples is 23.
 Find the sum of all the multiples of 3 or 5 below 1000.
 """
 
-import sys
 import argparse
+import sys
+
 
 def imperative_sum(limit):
-    ''' Straightforward sum calculation via loop and check '''
+    """ Straightforward sum calculation via loop and check """
+
     def _divisible_by_3_or_5(num):
         return num % 3 == 0 or num % 5 == 0
 
@@ -24,56 +26,79 @@ def imperative_sum(limit):
 
     return total
 
+
 def functional_sum(limit):
-    ''' Using functional programming concepts '''
-    return sum( set(xrange(0, limit, 3)) | set(xrange(0, limit, 5)) )
+    """ Using functional programming concepts """
+    return sum(set(xrange(0, limit, 3)) | set(xrange(0, limit, 5)))
+
 
 def algebraic_sum(limit):
-    ''' Calculates the sum by exploiting the fact that the sum of consecutive
-        integers 1 ... n equals 0.5 * n(n+1) '''
-    def _sum_1_to_n(n):
-        return (n * (n+1)) / 2
+    """ Calculates the sum by exploiting the fact that the sum of consecutive
+        integers 1 ... n equals 0.5 * n(n+1) """
 
-    return ( 3 * _sum_1_to_n((limit-1)/3)) + \
-           ( 5 * _sum_1_to_n((limit-1)/5)) - \
-           (15 * _sum_1_to_n((limit-1)/15))
+    def _sum_1_to_n(n):
+        return (n * (n + 1)) / 2
+
+    return (
+        (3 * _sum_1_to_n((limit - 1) / 3))
+        + (5 * _sum_1_to_n((limit - 1) / 5))
+        - (15 * _sum_1_to_n((limit - 1) / 15))
+    )
 
 
 def main(argv):
-    description = "Calculate the sum of all numbers that are divisible by " \
-            "either 3 or 5 up to a limit"
+    description = (
+        "Calculate the sum of all numbers that are divisible by "
+        "either 3 or 5 up to a limit"
+    )
     parser = argparse.ArgumentParser(description=description)
     group = parser.add_mutually_exclusive_group()
-    group.add_argument("-a", "--algebraic",
-            help="Use the algebraic method",
-            action="store_const", const="algebraic",
-            dest="method")
-    group.add_argument("-f", "--functional",
-            help="Use the functional method",
-            action="store_const", const="functional",
-            dest="method")
-    group.add_argument("-i", "--imperative",
-            help="Use the imperative method",
-            action="store_const", const="imperative",
-            dest="method")
-    parser.add_argument("-l", "--limit",
-            help="Indicate the upper limit of the list; defaults to 1000",
-            type=int, default=1000)
-    parser.add_argument("-v", "--verbose",
-            help="Verbose mode",
-            action="store_true")
+    group.add_argument(
+        "-a",
+        "--algebraic",
+        help="Use the algebraic method",
+        action="store_const",
+        const="algebraic",
+        dest="method",
+    )
+    group.add_argument(
+        "-f",
+        "--functional",
+        help="Use the functional method",
+        action="store_const",
+        const="functional",
+        dest="method",
+    )
+    group.add_argument(
+        "-i",
+        "--imperative",
+        help="Use the imperative method",
+        action="store_const",
+        const="imperative",
+        dest="method",
+    )
+    parser.add_argument(
+        "-l",
+        "--limit",
+        help="Indicate the upper limit of the list; defaults to 1000",
+        type=int,
+        default=1000,
+    )
+    parser.add_argument("-v", "--verbose", help="Verbose mode", action="store_true")
 
     args = parser.parse_args()
 
-    sum_methods = { 'algebraic' : algebraic_sum,
-                    'functional': functional_sum,
-                    'imperative': imperative_sum, }
+    sum_methods = {
+        "algebraic": algebraic_sum,
+        "functional": functional_sum,
+        "imperative": imperative_sum,
+    }
 
     f = sum_methods.get(args.method, algebraic_sum)
 
     msg = "The total is {}" if args.verbose else "{}"
 
-    print msg.format( f(args.limit) )
+    print(msg.format(f(args.limit)))
 
 
 if __name__ == "__main__":
